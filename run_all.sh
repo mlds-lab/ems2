ee5614e39b21f9335ed49477881d8cb596366240
erisinger <erisinger@umass.edu>
2018-05-16 17:21:00 -0400

    run_all

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,9 @@
+# run summarization
 sh run_master_summarize.sh
 
+# run mobile/daily and initial indicator experiments
 sh run_in_serial.sh master_summary_dataframe.pkl ./data/experiment_definitions/daily/
 sh run_in_serial.sh master_summary_dataframe.pkl ./data/experiment_definitions/initial/
 
+# generate CSV results
 python3 standalone_report_generator.py --data-directory ./experiment_results/ --experiment-name basic
